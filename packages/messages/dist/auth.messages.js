c6cf39bd3c90688fc4db7d57728e8f354b7fe264
DorianLeci <dleci00@fesb.hr>
2026-05-05 19:23:54 +0200

    Delete packages/messages/dist directory

--- packages/messages/dist/auth.messages.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AUTH_MESSAGES = void 0;
-exports.AUTH_MESSAGES = {
-    INVALID_CREDENTIALS: 'Invalid email or password',
-    EMAIL_EXISTS: 'Email already exists',
-    UNAUTHORIZED: 'Unauthorized access',
-};
