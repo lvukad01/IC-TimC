8024061edbce54da77c43661d4d41994d5a7265b
DorianLeci <dleci00@fesb.hr>
2026-05-07 16:42:56 +0200

    Update "UsersService" with geocoding integration, Update dto typization

--- packages/types/src/dto/user-dto.ts
+++ packages/types/src/dto/user-dto.ts
@@ -14,3 +14,16 @@ export interface UserResponse {
   phone?: string;
   role: string;
 }
+
+export interface UserResponse {
+  id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  phone?: string;
+  role: string;
+  street: string;
+  city: string;
+  zipcode: string;
+  country: string;
+}
